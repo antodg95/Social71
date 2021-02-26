4cb7b5cb01049e7f3129c23342418ff87aea5e90
antonio.digiulio <antonio.digiulio@alten.it>
2021-02-26 11:31:03 +0100

    Initial commit

---
+++ src/main/java/it/digiulio/social71/Social71Application.java
@@ -0,0 +1,13 @@
+package it.digiulio.social71;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Social71Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Social71Application.class, args);
+    }
+
+}
---
+++ src/test/java/it/digiulio/social71/Social71ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.digiulio.social71;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Social71ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
