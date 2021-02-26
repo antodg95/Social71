64c9ac0adcc0559f040853ef63f200773d14b1ce
antonio.digiulio <antonio.digiulio@alten.it>
2021-02-26 12:49:09 +0100

    users and whispers sql script

---
+++ scripts/create-001.sql
@@ -0,0 +1,17 @@
+CREATE TABLE users (
+    id serial,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    email varchar (255) UNIQUE NOT NULL,
+    created_on TIMESTAMP NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE whispers (
+    id SERIAL,
+    user_id INT NOT NULL,
+    text varchar(255) NOT NULL,
+    created_on TIMESTAMP NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
