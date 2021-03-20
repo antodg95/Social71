2b6930cf77d9ce3dfb763a7a55804c74e778c453
antodg95 <antodg95@gmail.com>
2021-03-20 19:02:31 +0100

    User create endpoint is available to unregistered ones

--- docs/todos.md
+++ docs/todos.md
@@ -1,6 +1,6 @@
 # TODOs:
 - Vedere come gestire meglio la password. Sia sui check di constraint e validation, sia quando viene ritornato l'user tramite JSON.
-- Permettere il create User del controller a tutti (ora permette solo se autenticati)
+- ~~Permettere il create User del controller a tutti (ora permette solo se autenticati)~~
 - Rendere il ritorno di una lista (ad esempio quella per la lista di Whispers di un utente) più descrittiva, ad esempio 
   {"resultSize": Long, "Whispers": [lista di Whispers]}
 - Rendere possibile la creazione di un utente Admin in qualche modo  
--- src/main/java/it/digiulio/social71/configuration/SecurityConfiguration.java
+++ src/main/java/it/digiulio/social71/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package it.digiulio.social71.configuration;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -35,6 +36,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/api/v1/users").permitAll();
+
         http.authorizeRequests()
                 .antMatchers("/api/v1/**").hasAnyRole("ADMIN", "USER");
 
